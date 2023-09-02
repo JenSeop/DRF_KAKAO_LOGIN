177e678988737ca3805f360a4c83778427d23f12
JenSeop <dostwoman@gmail.com>
2023-09-03 02:20:02 +0900

    [23.09.03] another app upload

---
+++ social_login/social_login/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('dj_rest_auth.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('accounts/', include('accounts.urls')),
+]
